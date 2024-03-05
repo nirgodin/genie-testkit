3696fe01b142309b808faffcd0e21cb9520566ef
nir.godin <nir.godin@planckresolution.com>
2024-03-05 09:58:17 +0200

    Fixed posgres testkit

--- genie_testkit/postgres_testkit.py
+++ genie_testkit/postgres_testkit.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from genie_common.tools import logger
 from genie_common.utils import random_alphanumeric_string
+from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from testcontainers.postgres import PostgresContainer
 
@@ -13,18 +14,19 @@ class PostgresTestkit:
                  port: Optional[int] = None,
                  user: Optional[str] = None,
                  password: Optional[str] = None,
-                 dbname: Optional[str] = None,
-                 driver: Optional[str] = None):
+                 dbname: Optional[str] = None):
         self.image = image or "postgres:15.6"
         self.port = port or 5432
         self.user = user or random_alphanumeric_string()
         self.password = password or random_alphanumeric_string()
         self.dbname = dbname or random_alphanumeric_string()
-        self.driver = driver or "asyncpg"
         self._container = container
 
-    def get_database_engine(self) -> AsyncEngine:
-        return create_async_engine(self._container.get_connection_url())
+    def get_database_engine(self, driver: str = "asyncpg") -> AsyncEngine:
+        raw_url = self._container.get_connection_url()
+        url = raw_url.replace("psycopg2", driver)
+
+        return create_async_engine(url=url, poolclass=NullPool)
 
     def __enter__(self) -> "PostgresTestkit":
         if self._container is None:
@@ -35,7 +37,7 @@ class PostgresTestkit:
                 user=self.user,
                 password=self.password,
                 dbname=self.dbname,
-                driver=self.driver
+                driver="psycopg2"
             )
             self._container.__enter__()
 
