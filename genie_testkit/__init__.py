3c3dd44694a68793a151aabfa0f95cd6c20f3632
nir.godin <nir.godin@planckresolution.com>
2024-03-28 19:45:34 +0200

    Added redis testkit

--- genie_testkit/__init__.py
+++ genie_testkit/__init__.py
@@ -1,5 +1,7 @@
 from genie_testkit.postgres_testkit import PostgresTestkit
+from genie_testkit.redis_testkit import RedisTestkit
 
 __all__ = [
-    "PostgresTestkit"
+    "PostgresTestkit",
+    "RedisTestkit"
 ]
